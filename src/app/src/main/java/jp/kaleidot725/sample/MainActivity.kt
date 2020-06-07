6fbd8f0ea7c56e2f5758bcdd0c527d48d70565cd
Kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2020-06-07 08:52:11 -0700

    Update

--- src/app/src/main/java/jp/kaleidot725/sample/MainActivity.kt
+++ src/app/src/main/java/jp/kaleidot725/sample/MainActivity.kt
@@ -14,24 +14,23 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
-    private val recognizeSpeechIntent : Intent get() =  Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH).apply {
-        this.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
-        this.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, applicationContext.packageName)
-    }
+    private var speechRecognizer : SpeechRecognizer? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         requestRecordAudioPermissions()
+        speechRecognizer = SpeechRecognizer.createSpeechRecognizer(applicationContext)
+        speechRecognizer?.setRecognitionListener(createRecognitionListenerStringStream { recognize_text_view.text = it })
+        recognize_start_button.setOnClickListener { speechRecognizer?.startListening(recognizeSpeechIntent) }
+        recognize_stop_button.setOnClickListener { speechRecognizer?.stopListening() }
+    }
 
-        val recognizer = SpeechRecognizer.createSpeechRecognizer(applicationContext)
-        recognizer.setRecognitionListener(createRecognitionListenerStringStream {
-            recognize_text_view.text = it
-        })
-        recognize_button.setOnClickListener {
-            recognizer.startListening(recognizeSpeechIntent)
-        }
+    override fun onDestroy() {
+        super.onDestroy()
+        speechRecognizer?.cancel()
+        speechRecognizer?.destroy()
     }
 
     private fun requestRecordAudioPermissions() {
@@ -40,6 +39,59 @@ class MainActivity : AppCompatActivity() {
             return
         }
         ActivityCompat.requestPermissions(this, arrayOf(RECORD_AUDIO), PERMISSIONS_RECORD_AUDIO)
+    }
+
+    private val recognizeSpeechIntent : Intent get() =  Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH).apply {
+        this.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
+        this.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, applicationContext.packageName)
+    }
+
+    private fun createRecognitionListenerStringStream(onResult : (String)-> Unit) : RecognitionListener {
+        return object : RecognitionListener {
+            // The sound level in the audio stream has changed.
+            override fun onRmsChanged(rmsdB: Float) {}
+
+            // Called when the endpointer is ready for the user to start speaking.
+            override fun onReadyForSpeech(params: Bundle) {
+                onResult("onReadyForSpeech")
+            }
+
+            // More sound has been received.
+            override fun onBufferReceived(buffer: ByteArray) {
+                onResult("onBufferReceived")
+            }
+
+            // Called when partial recognition results are available.
+            override fun onPartialResults(partialResults: Bundle) {
+                onResult("onPartialResults")
+            }
+
+            // Reserved for adding future events.
+            override fun onEvent(eventType: Int, params: Bundle) {
+                onResult("onEvent")
+            }
+
+            // The user has started to speak.
+            override fun onBeginningOfSpeech() {
+                onResult("onBeginningOfSpeech")
+            }
+
+            // Called after the user stops speaking.
+            override fun onEndOfSpeech() {
+                onResult("onEndOfSpeech")
+            }
+
+            // A network or recognition error occurred.
+            override fun onError(error: Int) {
+                onResult("onError")
+            }
+
+            // Called when recognition results are ready.
+            override fun onResults(results: Bundle) {
+                val stringArray = results.getStringArrayList(android.speech.SpeechRecognizer.RESULTS_RECOGNITION);
+                onResult("onResults " + stringArray.toString())
+            }
+        }
     }
 
     companion object {
